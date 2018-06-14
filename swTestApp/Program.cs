76047b93a5803c16459504eaec387a2f25381dd2
Billy Delgado <37461749+Bill7zz@users.noreply.github.com>
2018-06-14 20:29:27 -0300

    Uploading the main repository

---
+++ swTestApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+
+namespace swTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var apiUrl = "http://loripsum.net/api/1/short/prude";
+
+            var sampleText = GetTextFromApiLorem(apiUrl);
+
+            Console.WriteLine("Loren text from web API - loripsum.net ");
+        
+            Console.WriteLine(sampleText);
+
+        }
+ 
+        static string GetTextFromApiLorem(String url)
+        {           
+            WebRequest request = WebRequest.Create(url);
+
+            WebResponse response = request.GetResponse();
+
+            Stream dataStream = response.GetResponseStream();
+
+            var reader = new StreamReader(dataStream);
+            var textFromApi = reader.ReadToEnd();
+
+            reader.Close();
+            response.Close();
+
+            return textFromApi;      
+        }
+
+    }
+}
